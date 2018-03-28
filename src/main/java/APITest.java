a64e1204c1162480f8e2a0e92e237f0f86775c99
derrickliu <derkliu@gmail.com>
2018-03-28 12:39:17 -0500

    -Updated with key and new test url

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 
     /** Subscription key. Replace with your API key. */
     private static final String SUBSCRIPTION_KEY =
-            "13hc77781f7e4b19b5fcdd72a8df7156";
+            "95f346697c4b45e188e97b6a132b5660";
 
     /**
      * Region to use.
@@ -57,8 +57,7 @@ public class APITest {
 
             // Request body.
             StringEntity reqEntity =
-                    new StringEntity("{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/"
-                            + "1/12/Broadway_and_Times_Square_by_night.jpg\"}");
+                    new StringEntity("{\"url\":\"https://i.redd.it/k0x6px34wao01.jpg\"}");
             request.setEntity(reqEntity);
 
             // Execute the REST API call and get the response entity.
